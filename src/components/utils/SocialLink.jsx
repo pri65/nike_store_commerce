057b6633fdf9bc3465a9d0c6a827df61cf87d307
pri65 <priyankasoniii65@gmail.com>
2023-01-18 12:29:56 +0530

    Added social links

--- src/components/utils/SocialLink.jsx
+++ src/components/utils/SocialLink.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const SocialLink = () => {
+const SocialLink = ({ icon }) => {
   return (
-    <div>SocialLink</div>
+   <>
+      <img
+        src={icon}
+        alt="icon/social"
+        className="w-8 h-8 flex items-center cursor-pointer md:w-6 md:h-6 sm:w-5 sm:h-5 transition-all duration-200 hover:scale-110"
+      />
+   </>
   )
 }
 
