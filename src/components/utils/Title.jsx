bdb82c5b7e81504d6745a376508578c4610c165b
pri65 <priyankasoniii65@gmail.com>
2023-01-18 12:40:24 +0530

    Setting titles

--- src/components/utils/Title.jsx
+++ src/components/utils/Title.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Title = () => {
+const Title = ({ title }) => {
   return (
-    <div>Title</div>
+   <>
+      <div className='grid items-center'>
+        <h1 className='text-5xl lg:text-4xl md:text-3xl font-bold text-slate-900 filter
+         drop-shadow-lg'>{title}</h1>
+      </div>
+   </>
   )
 }
 
