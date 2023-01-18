efc162a3613df47f8050f83470da4c1351222d1e
pri65 <priyankasoniii65@gmail.com>
2023-01-18 12:17:36 +0530

    Folders structured

---
+++ src/app/CartSlice.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartSlice = () => {
+  return (
+    <div>CartSlice</div>
+  )
+}
+
+export default CartSlice
---
+++ src/components/FlexContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FlexContent = () => {
+  return (
+    <div>FlexContent</div>
+  )
+}
+
+export default FlexContent
