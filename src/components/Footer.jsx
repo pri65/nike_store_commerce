6a3019ca4857dd6fddf0c5db3a79c41a9ee79649
pri65 <priyankasoniii65@gmail.com>
2023-01-18 12:46:00 +0530

    Setting footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Footer = () => {
+const Footer = ({ footerAPI: { titles, links } }) => {
+  const [Year, setYear] = useState();
+  useEffect(() => {
+      const getYear = () => setYear(new Date().getFullYear());
+      getYear();
+  }, []);
   return (
-    <div>Footer</div>
+   <>
+      <footer className='bg-theme pt-7 pb-5'>
+        <div className='nike-container text-slate-200'>
+          <div className='grid items-start grid-cols-3 max-w-2xl w-full m-auto md:max-w-none md:gap-5'>
+            {titles.map((val, i) => (
+              <div key={i} className="grid items-center">
+                <h1 className='text-lg lg:text-base md:text-sm uppercase font-semibold'>{val.title}</h1>
+              </div>
+            ))}
+            {links.map((list, i) => (
+              <ul key={i} className="grid items-center gap-1">
+                {list.map((link, i) => (
+                  <li key={i} className="text-sm sm:text-xs">{link.link}</li>
+                ))}
+              </ul>
+            ))}
+          </div>
+          <div className='mt-5 text-center'>
+            <p className='text-sm md:text-center'>Copyright<sup className='text-base font-bold'>&copy;</sup> All Reserved Rights <span className='font-semibold'>JSSTACK DEVELOPERS {Year}</span></p>
+          </div>
+        </div>
+      </footer>
+   </>
   )
 }
 
